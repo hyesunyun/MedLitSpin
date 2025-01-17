2a9f79b1a99a5605c5f7ea16e6f4fc8c1928e670
hyesunyun <hyesunyun09@gmail.com>
2025-01-17 12:44:47 -0500

    add all results

--- run_generate_pls.sh
+++ run_generate_pls.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 # List of models to use
+# models=(
+#   "gpt4o" 
+#   "gpt4o-mini" 
+#   "claude_3.5-sonnet"
+#   "llama3_instruct-8B"
+#   "llama2_chat-70B"
+#   "llama3_instruct-70B"
+#   "med42-70B"
+#   "openbiollm-70B"
+# )
+
 models=(
-  "gpt4o" 
-  "gpt4o-mini" 
-  "claude_3.5-sonnet"
-  "llama3_instruct-8B"
-  "llama2_chat-70B"
-  "llama3_instruct-70B"
-  "med42-70B"
-  "openbiollm-70B"
+  "gpt35"
+  "gemini_1.5_flash"
+  "gemini_1.5_flash-8B"
+  "claude_3.5-haiku"
+  "olmo2_instruct-7B"
+  "olmo2_instruct-13B"
+  "mistral_instruct7B"
+  "med42-8B"
+  "biomistral7B"
+  "openbiollm-8B"
+  "llama2_chat-7B"
+  "llama2_chat-13B"
+  "biomedgpt7B"
+  "alpacare-7B"
 )
 
 # DEFAULT PROMPT TEMPLATE with 300 max new tokens
