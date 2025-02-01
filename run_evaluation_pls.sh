38e41ea3a6ecb35ee3f6fb41cd8092eb06c2ce94
hyesunyun <hyesunyun09@gmail.com>
2025-02-01 15:05:27 -0500

    update eval pls

--- run_evaluation_pls.sh
+++ run_evaluation_pls.sh
@@ -8,26 +8,26 @@ evaluators=(
 models=(
   "gpt35"
   "gpt4o"
-  # "gpt4o-mini"
-  # "gemini_1.5_flash"
-  # "gemini_1.5_flash-8B"
-  # "claude_3.5-sonnet"
-  # "claude_3.5-haiku"
-  # "olmo2_instruct-7B"
-  # "olmo2_instruct-13B"
-  # "mistral_instruct7B"
-  # "med42-8B"
-  # "biomistral7B"
-  # "openbiollm-8B"
-  # "llama2_chat-7B"
-  # "llama2_chat-13B"
-  # "llama3_instruct-8B"
-  # "llama2_chat-70B"
-  # "llama3_instruct-70B"
-  # "med42-70B"
-  # "openbiollm-70B"
-  # "biomedgpt7B"
-  # "alpacare-7B"
+  "gpt4o-mini"
+  "gemini_1.5_flash"
+  "gemini_1.5_flash-8B"
+  "claude_3.5-sonnet"
+  "claude_3.5-haiku"
+  "olmo2_instruct-7B"
+  "olmo2_instruct-13B"
+  "mistral_instruct7B"
+  "med42-8B"
+  "biomistral7B"
+  "openbiollm-8B"
+  "llama2_chat-7B"
+  "llama2_chat-13B"
+  "llama3_instruct-8B"
+  "llama2_chat-70B"
+  "llama3_instruct-70B"
+  "med42-70B"
+  "openbiollm-70B"
+  "biomedgpt7B"
+  "alpacare-7B"
 )
 
 echo "Running evaluation for interpreting trial results from vanilla PLS using the evaluator models..."
@@ -37,6 +37,6 @@ for evaluator in "${evaluators[@]}"; do
     python3 code/run_spin_interpretation_from_pls_evaluation.py \
       --model "$evaluator" \
       --input_path "code/pls_outputs/$model/${model}_outputs.csv" \
-      --output_path "code/pls_outputs/_interpretation_eval_results/$evaluator/$model" --debug
+      --output_path "code/pls_outputs/_interpretation_eval_results/$evaluator/$model"
   done
 done
--- sbatch_run_evaluation_pls.sh
+++ sbatch_run_evaluation_pls.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=60:00:00
 #SBATCH --job-name=spin_eval
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=50G
-#SBATCH --partition=short
+#SBATCH --partition=frink
 #SBATCH -o output_%j.txt                     # Standard output file
 #SBATCH -e error_%j.txt                      # Standard error file
 #SBATCH --mail-user=yun.hy@northeastern.edu  # Email
@@ -13,11 +13,12 @@
 
 # Your program/command here
 module purge
-module load explorer anaconda3/2024.06
+# module load explorer anaconda3/2024.06
+module load anaconda3/3.6
 
-source activate base
+# source activate base
 source activate MedLitSpin
-conda activate MedLitSpin
+# conda activate MedLitSpin
 
 conda info
 
